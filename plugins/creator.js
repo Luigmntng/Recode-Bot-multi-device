7131edf37645b3cc340039a15565a1a91c321aa8
Luigmntng <97533143+Luigmntng@users.noreply.github.com>
2022-04-05 18:04:57 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,11 +1,18 @@
 const PhoneNumber = require('awesome-phonenumber')
 async function handler(m) {
-                let vcard = 'BEGIN:VCARD\n' // metadata of the contact card
-                    + 'VERSION:3.0\n' 
-                    + 'N:;Lui gmntng;;;'
-                    + 'FN:Lui gmntng\n' // full name
-                    + 'ORG:kalo mau masukin bot ini ke gc lu chat;\n' // the organization of the contact
-                    + 'TEL;type=CELL;type=VOICE;waid=6282146092695:+62 821-4609-2695\n' // WhatsApp ID + phone number
+                let vcard ='BEGIN:VCARD\n'
+                    + 'VERSION:3.0\n'
+                    + 'N:;Lui;;;\n'
+                    + 'FN:Lord Lui\n'
+                    + 'item1.TEL;waid=6282146092695:6282146092685\n'
+                    + 'item1.X-ABLabel:📍 Creator\n'
+                    + 'item2.EMAIL;type=INTERNET:Kodamchina@hacked.khom\n'
+                    + 'item2.X-ABLabel:💌 Email\n'
+                    + 'item3.URL:https://google.xyz/\n'
+                    + 'item3.X-ABLabel:📮 Rest Api\n'
+                    + 'item4.ADR:;;🇮🇩 Indonesia;;;;\n'
+                    + 'item4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Region | Otaku 🇯🇵\n'
+                    + 'item5.X-ABLabel:───────[ Lord Lui ]───────\n'
                     + 'END:VCARD'
                 conn.sendMessage(m.chat, { contacts: { displayName: 'Lui gmntng', contacts: [{ vcard }] } }, { quoted: m })
 }
@@ -15,3 +22,4 @@ handler.tags = ['info']
 handler.command = /^(owner|creator)$/i
 
 module.exports = handler
+'
